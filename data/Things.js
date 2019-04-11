a5e14e2917619da0435af486a87978b9ce4df922
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-04-11 16:56:46 -0400

    add in-memory model

---
+++ data/Things.js
@@ -0,0 +1,36 @@
+const Promise = require('promise');
+
+let lastId = 7;
+let Things = [
+  { id: 1, name: 'Test1' },
+  { id: 2, name: 'Test2' },
+  { id: 3, name: 'Test3' },
+  { id: 4, name: 'Test4' },
+  { id: 5, name: 'Test5' },
+  { id: 6, name: 'Test6' },
+  { id: 7, name: 'Test7' },
+];
+
+const generateId = () => {
+  lastId = lastId + 1;
+  return lastId;
+};
+
+module.exports = {
+  insert: thing => {
+    const newThing = { ...thing, id: generateId() };
+    Things.push(newThing);
+    return Promise.resolve(newThing);
+  },
+  delete: id => {
+    const foundIndex = Things.indexOf(t => t.id === id);
+    if (foundIndex === -1) {
+      return Promise.resolve(null);
+    }
+
+    const deletedItem = Things[foundIndex];
+    Things = Things.splice(foundIndex, 1);
+    return Promise.resolve(deletedItem);
+  },
+  Things
+}
